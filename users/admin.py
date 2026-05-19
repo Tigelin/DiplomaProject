b8de2640227d7db01b940305da20f59a2c2ae2b6
Tigelin <151899316+Tigelin@users.noreply.github.com>
2026-05-19 13:39:19 +1000

    Дополнительные разделы Users и Roles в админке

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Role
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'role', 'is_staff')
+    list_filter = ('role', 'is_staff')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Дополнительно', {'fields': ('role', 'phone')}),
+    )
