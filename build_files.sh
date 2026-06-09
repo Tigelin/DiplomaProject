03c46a6e0bbb6d8966cde947dae7c07eb5c0a98b
Tigelin <151899316+Tigelin@users.noreply.github.com>
2026-06-09 16:22:01 +1000

    Ещё попытки починить сбор статики.

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,9 @@
-echo "Building project..."
+#!/bin/bash
+echo "Installing dependencies..."
 pip install -r requirements.txt
+
+echo "Collecting static files..."
 python manage.py collectstatic --noinput --clear
+
+echo "Build completed!"
+ls -la staticfiles/
--- core/settings.py
+++ core/settings.py
@@ -136,6 +136,9 @@ AUTH_USER_MODEL = 'users.User'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
