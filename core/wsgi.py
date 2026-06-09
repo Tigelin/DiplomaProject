b11dc171b857b21b3ad3b352290a7313c59db668
Tigelin <151899316+Tigelin@users.noreply.github.com>
2026-06-09 16:26:58 +1000

    Третья попытки починить сбор статики.

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,17 +1,20 @@
-"""
-WSGI config for core project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
-app = application
+
+try:
+    from whitenoise import WhiteNoise
+    
+    static_root = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'staticfiles')
+    
+    if not os.path.exists(static_root):
+        os.makedirs(static_root, exist_ok=True)
+    
+    application = WhiteNoise(application, root=static_root)
+    application.add_files(static_root, prefix='static/')
+    
+except Exception as e:
+    print(f"WhiteNoise warning: {e}")
