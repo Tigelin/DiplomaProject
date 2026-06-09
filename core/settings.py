19bb73e9680c342b3a3e480bbc638b7cf63ab7f9
Tigelin <151899316+Tigelin@users.noreply.github.com>
2026-06-09 16:14:54 +1000

    Правки settings.py для сбора static на хостинге.

--- core/settings.py
+++ core/settings.py
@@ -134,7 +134,7 @@ AUTH_USER_MODEL = 'users.User'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
